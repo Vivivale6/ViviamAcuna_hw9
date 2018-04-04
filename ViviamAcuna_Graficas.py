ca67d48eb8250b0ef6cd4fbc7785fd63e9444583
Vivivale6 <37335436+Vivivale6@users.noreply.github.com>
2018-04-04 15:33:45 -0500

    Add files via upload

---
+++ ViviamAcuna_Graficas.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+py=np.loadtxt(times_python.csv)
+c_mas=np.loadtxt(times_cpp)
+
+n1=py[:,0]
+ti=py[:,1]
+
+n2=c_mas[:,0]
+ti2=c_mas[:,1]
+
+
+plt.plot(ti,n1)
+
+plt.plot(ti2,n2)
+plt.savefig("cpp_vs_python.png")
